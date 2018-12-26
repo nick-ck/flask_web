876759c84242166153934fc39e828a7c64505c91
nick-ck <314610796@qq.com>
2018-12-26 14:21:10 +0800

    function user

--- hello.py
+++ hello.py
@@ -1,7 +1,10 @@
-from flask import Flask
-
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "<h1>hello,world</h1>"
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html',name=name)
